51c08d6abc167868efc0269a16f6cd7d9f147784
adrian-leyva14 <adrianernestoleyvagodinez@gmail.com>
2024-09-26 14:14:03 -0700

    Add files via upload

---
+++ CONVERSION DE GRADOS/main.cpp
@@ -0,0 +1,17 @@
+/*COVERSION DE GRADOS*/
+
+#include <iostream> // usamos espacio de nombre estandar cout, salida, cin
+//esntrada, cerr, salida de errores, clog se usa para mensaje de logical_and
+
+int main() {
+    float celsius, fahrenheint;
+
+    cout << "Grados Celsius: ";
+    cin >> Celsius;
+
+    fahrenheint = (celsius * 9 / 5) + 32;
+
+    cout << celsius << "°C son " << fahrenheit << "°F" <<endl;
+
+    return 0;
+}
